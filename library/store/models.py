b849b334f2cf43598a315e874df5e39529f26ad8
Denis9505 <victoria_l98@mail.ru>
2023-01-14 16:26:46 +0300

    Added filters

--- library/store/models.py
+++ library/store/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Book(models.Model):
+    
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    author = models.CharField(max_length=100)
 
     def __str__(self) -> str:
-        return f'{self.title}'
+        return f'Id:{self.id} {self.title}'
--- library/store/views.py
+++ library/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Book
 from .serializers import BooksSerializer
@@ -8,3 +10,7 @@ from .serializers import BooksSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ['price']
+    search_fields = ['author', 'title']
+    ordering_fields = ['price', 'author']
